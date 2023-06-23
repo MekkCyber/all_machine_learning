213e1c6428aac39d4973d3cc9878772a62042043
MekkCyber <mekk.cyber@gmail.com>
2023-06-23 15:53:31 +0200

    k_means start

---
+++ k_means/k_means.py
@@ -0,0 +1,23 @@
+import seaborn as sns
+import matplotlib.pyplot as plt 
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+centers = 5
+X_train, y_train = make_blobs(n_samples=100, centers=5, random_state=42)
+X_train = scaler.fit_transform(X_train)
+sns.scatterplot(x=X_train[:,0],y=X_train[:,1],hue=y_train,palette="deep")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+class KMeans : 
+    
+    def __init__(self, number_clusters=5) : 
+        self.n_clusters = number_clusters
+    
+    def initialize(self,X) : 
+
+        self.centroids = 
